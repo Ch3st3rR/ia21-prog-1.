775d30060b77481b7a8a2f4580907edc45fcff89
Enzo <104568709+Ch3st3rR@users.noreply.github.com>
2022-05-15 11:03:00 -0300

    Add files via upload

---
+++ att 1/att 2.mjs
@@ -0,0 +1,20 @@
+let x = true;
+let nome = []
+let oque = []
+let eisso = [] 
+while (x) {
+     console.log("dados:", nome, oque)
+    const nome = await r1.question('João') 
+    const nome = await r1.question('o que diabos') 
+    const nome = await r1.question('é isso?') 
+    const falas = await r1.question("continue com as falas")
+    
+    console.log(eisso.nome)
+
+    eisso.Ah, para.()
+
+    console.log(eisso.nome) 
+
+    eisso.pq, sim.()
+
+}
